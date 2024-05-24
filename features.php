3d5938fec4c79600529ceac8061247fef3294f93
Hikam-Baidawih <hikambaidawih@gmail.com>
2024-05-25 02:30:00 +0700

    memeberikan beberapa pesan

--- features.php
+++ features.php
@@ -1,7 +1,7 @@
 <?php
 
     $features = array("Melihat password","melihat nama member","memposting iklan","memeberi like", "memberi rating pada artikel");
-    echo"fitur yang ada pada apk halo petani adalah";
+    echo"fitur yang ada pada apk halo petani adalah diantara berikut";
 
     foreach ($features as $feature) {
         echo "feature {$feature}";
