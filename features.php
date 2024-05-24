568d76577176f9cfbc35c62e153912e0c82518ec
Hikam-Baidawih <hikambaidawih@gmail.com>
2024-05-25 02:39:10 +0700

    memberikan tambahan fitur

--- features.php
+++ features.php
@@ -1,6 +1,6 @@
 <?php
 
-    $features = array("Melihat password","melihat nama member","memposting iklan","memeberi like", "memberi rating pada artikel");
+    $features = array("Melihat password","melihat nama member","memposting iklan","memeberi like", "memberi rating pada artikel", "delete member");
     echo"fitur yang ada pada apk halo petani adalah diantara berikut";
 
     foreach ($features as $feature) {
