92d214b1d0d6a688e3e86cbe6f375369243a9363
Hikam-Baidawih <hikambaidawih@gmail.com>
2024-05-25 02:15:16 +0700

    menambah file features.php

---
+++ features.php
@@ -0,0 +1,10 @@
+<?php
+
+    $features = array("Melihat password","melihat nama member","memposting iklan","memeberi like");
+    echo"fitur yang ada pada apk halo petani adalah";
+
+    foreach ($features as $feature) {
+        echo "feature {$feature}";
+    }
+
+?>
