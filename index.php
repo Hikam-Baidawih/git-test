238bb182a6567267f7c1a2c891fe98cba2883421
Hikam-Baidawih <hikambaidawih@gmail.com>
2024-05-25 01:24:29 +0700

    menambah variabel makanan pada file php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
     $nama = "Hikam";
     $umur =  20;
+    $makanan = "Burger";
     echo"HALO NAMA AKU {$nama} <br>";
     echo"AKU BERUMUR {$umur} <br>";
+    echo"AKU SUKA {$makanan} <br>";
     
 ?>
